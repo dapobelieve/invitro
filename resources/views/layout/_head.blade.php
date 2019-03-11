eab3df069ff55212ed444f5162c9707d502ffa90
dapobelieve <dapomichaels@gmail.com>
2019-03-11 15:00:03 +0100

    template

---
+++ resources/views/layout/_head.blade.php
@@ -0,0 +1,13 @@
+<head>
+    <title>@yield('page.description')</title>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/animations.css">
+    <link rel="stylesheet" href="/assets/css/fonts.css">
+    <link rel="stylesheet" href="/assets/css/main.css" class="color-switcher-link">
+    <link rel="stylesheet" href="/assets/css/shop.css">
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+</head>
